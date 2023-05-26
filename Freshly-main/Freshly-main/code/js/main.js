405438d52eff2be7aaee4f5330dcf04d2a170141
Euloge Agnoun <obafemieulogeagnoun@gmail.com>
2023-05-26 19:52:20 +0200

    Freshly Restaurant

---
+++ Freshly-main/Freshly-main/code/js/main.js
@@ -0,0 +1,11 @@
+// Chargement du plugin sur le parent
+
+$(function () {
+    $(".slick").slick({
+        slidesToShow: 1,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+
+    });
+});
